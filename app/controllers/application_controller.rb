92f2edc874e78f5871408d7ac54bc5cf039803f7
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-06-11 08:24:33 +0900

    add gem devise,bootstrap,jquery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+    # ログイン確認
+    #  ログインしていない場合は、ログイン認証の画面へ遷移
+    before_action :authenticate_user!
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+
+  # welcomeページへの遷移のみは未ログインでも可とする
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
   end
 end
